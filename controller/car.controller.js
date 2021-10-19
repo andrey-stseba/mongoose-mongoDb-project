5ad50de3e0761030b58acff7d49bb4260b00a6b4
andrey-stseba <scebaandru@gmail.com>
2021-10-19 20:16:19 +0300

    add server,app,test,router

---
+++ controller/car.controller.js
@@ -0,0 +1,73 @@
+const createError = require('http-errors');
+const { Car } = require('./../models');
+
+module.exports.createCar = async (req, res, next) => {
+  const { body } = req;
+
+  try {
+    const newCarInstanse = new Car(body);
+    const createdCar = await newCarInstanse.save();
+
+    if (createdCar) {
+      return res.status(200).send({ data: createdCar });
+    }
+    next(createError(400, 'Bad request'));
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getCars = async (req, res, next) => {
+  try {
+    const foundCars = await Car.find().limit(3);
+
+    res.status(200).send({ data: foundCars });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getCarById = async (req, res, next) => {
+  const {
+    params: { carId },
+  } = req;
+  try {
+    const foundCar = await Car.findById(carId);
+    if (foundCar) {
+      return res.status(200).send({ data: foundCar });
+    }
+    next(createError(404, 'User not found'));
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateCarById = async (req, res, next) => {
+  const {
+    params: { carId },
+  } = req;
+  try {
+    const updateCar = await Car.findByIdAndUpdate(carId);
+    if (updateCar) {
+      return next();
+    }
+    next(createError(404, 'User not found'));
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteCarById = async (req, res, next) => {
+  const {
+    params: { carId },
+  } = req;
+  try {
+    const deletedCar = await Car.findByIdAndDelete(carId);
+    if (deletedCar) {
+      return res.status(200).send({ data: deletedCar });
+    }
+    next(createError(404, 'User not found'));
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ routes/carRouter.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const { carController } = require('../controller');
+
+const carRouter = Router();
+
+carRouter
+  .route('/')
+  .get(carController.getCars)
+  .post(carController.createCar);
+
+carRouter
+  .route('./:carId')
+  .get(carController.getCarById)
+  .patch(carController.updateCarById, carController.getCarById)
+  .delete(carController.deleteCarById);
+
+module.exports = carRouter;
